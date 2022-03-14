83ace9ea87051565e1e68d80e23b8c2f76fd0985
Arafetki <93081181+Arafetki@users.noreply.github.com>
2022-03-15 00:44:36 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <unordered_map>
+#include <deque>
+using namespace std;
+
+class LRUcache{
+private:
+    unordered_map<int,int> cache;
+    int Capacity;
+    deque<int>dq;
+public:
+    LRUcache(int c):Capacity(c){
+    }
+    int get(int key) {
+        // not found
+        if (cache.find(key) == cache.end()) {
+            return -1;
+        } else {
+            // found
+            deque<int>::iterator it = dq.begin();
+            while (*it != key) {
+                it++;
+            }
+
+            // update queue: update it to most recent used value
+            dq.erase(it);
+            dq.push_front(key);
+
+            return cache[key];
+        }
+    }
+    void put(int key,int value){
+        if(cache.find(key)==cache.end()){
+            if (dq.size()==Capacity){
+
+                int last=dq.back();
+                dq.pop_back();
+                cache.erase(last);
+            }
+        }
+        else{
+            deque<int>::iterator it = dq.begin();
+            while (*it != key) {
+                it++;
+            }
+            dq.erase(it);
+            cache.erase(key);
+
+
+        }
+        dq.push_front(key);
+        cache[key] = value;
+    }
+
+
+};
+
+
+
